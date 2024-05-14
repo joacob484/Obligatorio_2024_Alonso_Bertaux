7fb1daa813ef8be42ec7b471b5254c3a58016b20
Joaquin Bertaux <joaco@MacBook-Air-de-Joaquin.local>
2024-05-14 13:52:33 -0300

    Creo primeras Clases

---
+++ ListaEnlazada.java
@@ -0,0 +1,17 @@
+public class ListaEnlazada<T> implements ListaEnlazadaInterface<T>{
+
+    @Override
+    public void add(T value) {
+
+    }
+
+    @Override
+    public void remove(int posicion) {
+
+    }
+
+    @Override
+    public T get(int position) {
+        return null;
+    }
+}
---
+++ ListaEnlazadaInterface.java
@@ -0,0 +1,11 @@
+public interface ListaEnlazadaInterface<T> {
+
+    public void add(T value);
+
+    public void remove(int posicion);
+
+    public T get(int position);
+
+
+
+}

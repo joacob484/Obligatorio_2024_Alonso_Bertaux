0eaa2d2dc402b0836846804ecc7eaf4861388bb0
Joaquin Bertaux <joaco@MacBook-Air-de-Joaquin.local>
2024-05-22 13:33:40 -0300

    Termino de dejar pronto los test

--- um/edu/uy/ArbolBinario/TestArbol.java
+++ um/edu/uy/ArbolBinario/TestArbol.java
@@ -1,4 +1,14 @@
 package um.edu.uy.ArbolBinario;
 
+import org.junit.Test;
+
 public class TestArbol {
+        MyArbolInterface<Integer,Integer> ArbolTest = new ArbolBinario<>();
+
+        @Test
+        public void PruebasArbol(){
+
+
+        }
+
 }
--- um/edu/uy/Stack/TestStack.java
+++ um/edu/uy/Stack/TestStack.java
@@ -1,4 +1,15 @@
 package um.edu.uy.Stack;
 
+import org.junit.Test;
+import um.edu.uy.Lista.ListaEnlazada;
+
 public class TestStack {
+
+    MyStackInterface<Integer> stackPrueba = new ListaEnlazada<>();
+
+    @Test
+    public void StackPrueba(){
+
+
+    }
 }
